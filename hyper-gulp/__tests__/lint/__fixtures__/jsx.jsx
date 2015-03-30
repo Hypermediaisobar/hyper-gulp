c7940ca62301aaf49f59b533575f3ecc67d6504c
Mateusz Charytoniuk <mateusz.charytoniuk@gmail.com>
2015-03-30 09:05:38 +0200

    rename gore-gulp to hyper-gulp everywhere

--- hyper-gulp/__tests__/lint/__fixtures__/jsx.jsx
+++ hyper-gulp/__tests__/lint/__fixtures__/jsx.jsx
@@ -1,6 +1,9 @@
 /**
- * @license Copyright (c) 2015, goreutils.com
- * For licensing, see LICENSE
+ * Copyright (c) 2015-present, Hypermedia Isobar
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 /*global document: false */
